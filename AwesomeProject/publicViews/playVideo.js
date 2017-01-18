21bc9547ca078ca5bb760bc986dd6045f66d90b2
LittleCurry <2441611062@qq.com>
2017-01-18 10:19:11 +0800

    import react-native-video-player

--- AwesomeProject/publicViews/playVideo.js
+++ AwesomeProject/publicViews/playVideo.js
@@ -9,7 +9,10 @@ import {
     Video
 } from 'react-native';
 
-import { Video } from 'react-native-video';
+// import { Video } from 'react-native-video';
+// import VideoPlayer from 'react-native-video-player';
+
+// const VIMEO_ID = '179859217';
 
 
 
@@ -18,13 +21,16 @@ class Vlcplayer extends Component {
 
     render() {
         const uri = 'http://cdn.goluk.cn/video/t1_2.mp4';
+        /*
+         <SimpleVideo uri={uri} buttonSize={50} />
+         <Text>A Simple Player</Text>
+         <View style={{marginTop:50}} />
+         <Video uri={uri} />
+         <Text>A full-featured player</Text>
+         */
         return (
             <View>
-                <SimpleVideo uri={uri} buttonSize={50} />
-                <Text>A Simple Player</Text>
-                <View style={{marginTop:50}} />
-                <Video uri={uri} />
-                <Text>A full-featured player</Text>
+
             </View>
         );
     }
